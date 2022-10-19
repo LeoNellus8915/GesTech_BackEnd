305161e8b5e4fb9257b6fabd9a8aabab1350d76e
LeoNellus8915 <101563460+LeoNellus8915@users.noreply.github.com>
2022-10-19 10:10:19 +0200

    commenti candidati

--- src/main/java/it/teorema/gestech/service/CommentiCandidatiService.java
+++ src/main/java/it/teorema/gestech/service/CommentiCandidatiService.java
@@ -14,9 +14,9 @@ import it.teorema.gestech.model.mapper.AllCommentiCandidato;
 public interface CommentiCandidatiService extends JpaRepository <CommentiCandidati, Integer> {
 	@Query("select cc.data as data, p.nome as nome, p.cognome as cognome, cc.note as note "
 			+ "from CommentiCandidati cc, Persone p "
-			+ "where p.id = cc.idPersona and cc.idPersona = :idPersona "
+			+ "where p.id = cc.idPersona and cc.idDettaglioCandidato = :idCandidato "
 			+ "order by cc.data desc")
-	List<AllCommentiCandidato> findByIdCandidato(int idPersona);
+	List<AllCommentiCandidato> findByIdCandidato(int idCandidato);
 	
 	@Modifying
 	@Transactional
